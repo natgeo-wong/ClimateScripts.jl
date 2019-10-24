4a1d19e59c470f3ffeb2a4fbbe45e7f7fb1df34b
natgeo-wong <natgeo.wong@outlook.com>
2019-10-23 23:00:25 -0400

    Memory requirements resolved.

--- mimic_download.jl
+++ mimic_download.jl
@@ -7,8 +7,4 @@ yr = Dates.year(nw); mo = Dates.month(nw); dy = Dates.day(nw);
 dvec = collect(Date(2016,10,1):Day(1):Date(yr,mo,dy))
 
 # 2) Download data for range of dates
-@info "$(Dates.now()) - System has $(Sys.free_memory()/2^20)MB of memory left.";
-for datei in dvec;
-    mimicrun(datei,clisatroot(),["SEA","IND"]); clearclustermem();
-    @info "$(Dates.now()) - System has $(Sys.free_memory()/2^20)MB of memory left.";
-end
+for datei in dvec; mimicrun(datei,clisatroot(),["SEA","IND"]); end
