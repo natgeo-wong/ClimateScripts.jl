f19e825a0b992ef4b4a28b9832576c54027c55d4
natgeo-wong <natgeo.wong@outlook.com>
2019-10-29 01:05:36 -0400

    Added eradownload to the test and increased variety of tests.

--- eratests.jl
+++ eratests.jl
@@ -1,3 +1,25 @@
 using ClimateERA
 
 init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(1,1,0,1,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(2,1,0,1,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(1,3,0,1,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(2,3,0,1,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(1,1,0,3,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(2,1,0,3,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(1,3,0,3,init);
+eradownload(emod,epar,ereg,time,eroot)
+
+init,eroot = erastartup(1,1); emod,epar,ereg,time = erainitialize(2,3,0,3,init);
+eradownload(emod,epar,ereg,time,eroot)
