da92e4b718c1999a7bbe570f11e24417b3158943
natgeo-wong <natgeo.wong@outlook.com>
2019-11-24 17:30:13 -0500

    Updating scripts from server test to GitHub repo.

--- eossg_resort.jl
+++ eossg_resort.jl
@@ -2,4 +2,4 @@ using ClimateGNSS, ClimateEasy
 global_logger(ConsoleLogger(stderr,Logging.Info))
 
 stn = eosload(7);
-gnssresort("EOS-SG","SMT","/n/holylfs/kuangdss01/users/nwong/data/")
+gnssresort("EOS-SG","SMT","/n/kuangdss01/users/nwong/data/")
